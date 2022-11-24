de984a88b0f9ee01e51963887341d35e44f3e651
MichaelSinn <michaelsinn.contact@gmail.com>
2022-11-24 13:27:06 -0500

    Added mutation queries

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,81 +1,62 @@
 import { gql } from '@apollo/client';
 
 export const ADD_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+      addUser(username: $username, email: $email, password: $password) {
+        token
+        user {
+          username
+          email
+          _id
+        }
       }
     }
-  }
 `;
 
-export const ADD_GAME =  gql`
-    mutation addToLibrary($gameId: String!) {
-        addToLibrary(gameId: $gameId){
-            token
-            game{
-                _id: ID
-                gameId: String
-                name: String
-                rating: Int
-                image: String
-                genre: String
-                storeLink: String
-                description: String
-            }
+export const ADD_GAME = gql`
+    mutation Mutation($game: GameInput!) {
+      addToLibrary(game: $game) {
+        email
+        username
+        savedGames {
+          _id
         }
-    }`
+      }
+    }
+`;
 
 export const REMOVE_GAME = gql`
-    mutation removeFromLibrary($gameId: String!){
-        removeFromLibrary(gameId: $gameId){
-            token 
-            game{
-                _id: ID
-                gameId: String
-                name: String
-                rating: Int
-                image: String
-                genre: String
-                storeLink: String
-                description: String
-            }
+    mutation RemoveFromLibrary($gameId: String!) {
+      removeFromLibrary(gameId: $gameId) {
+        email
+        username
+        savedGames {
+          _id
         }
-    }`
+      }
+    }
+`;
 
 export const ADD_WISH = gql`
-    mutation addToWishlist($gameId: String!){
-        addToWishlist(gameId: $gameId){
-            token
-            game{
-                _id: ID
-                gameId: String
-                name: String
-                rating: Int
-                image: String
-                genre: String
-                storeLink: String
-                description: String
-            }
+    mutation addToWishlist($game: GameInput!) {
+      addToWishlist(game: $game) {
+        email
+        username
+        wishList {
+          _id
         }
-    }`
+      }
+    }
+`;
 
 export const REMOVE_WISH = gql`
-    mutation removeFromWishlist($gameId: String!){
-        removeFromWishlist(gameId: $gameId){
-            token
-            game{
-                _id: ID
-                gameId: String
-                name: String
-                rating: Int
-                image: String
-                genre: String
-                storeLink: String
-                description: String
-            }
+    mutation removeFromWishlist($gameId: String!) {
+      removeFromWishlist(gameId: $gameId) {
+        email
+        username
+        wishList {
+          _id
         }
-    }`
+      }
+    }
+`;
