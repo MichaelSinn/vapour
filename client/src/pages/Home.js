f316d3679b6183b9f8f63e47ee99b2d5b0900d55
MichaelSinn <michaelsinn.contact@gmail.com>
2022-11-26 14:59:36 -0500

    Refactored some of the code to clean up

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,17 +6,19 @@ import { Box, Block } from 'react-bulma-components';
 
 //Components used on this page
 import GamesList from '../components/GamesList';
+import {searchGenre} from '../utils/API';
 
 // Home will show the top 5 most popular Genres and their games
 // by passing those props through the GamesList (and Game) component
 export default function Home() {
+    const genres = searchGenre(1, 1);
     return (
         <Box>
             <Block> { /** TODO: pass props for popular genres */}
-                <GamesList games={null}></GamesList>
+                <GamesList games={genres}></GamesList>
             </Block>
             <Block>{ /** TODO: pass props for the other genres and repeat this Block for each genre we want to display */}
-                <GamesList games={null}></GamesList>
+                <GamesList games={genres}></GamesList>
             </Block>
             <p>Home</p>
         </Box>
