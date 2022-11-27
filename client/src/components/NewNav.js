797ea63bf19e802d1aa1510f911f14f4c2a19f83
DylanGodfrey <39961099+DylanGodfrey@users.noreply.github.com>
2022-11-27 16:49:58 -0500

    Added burger menu for mobile view

--- client/src/components/NewNav.js
+++ client/src/components/NewNav.js
@@ -17,43 +17,53 @@ export default function NewNav({ genres, gameCount, userID }) {
     textDecoration: "underline",
   };
 
+  const toggleBurger = () => {
+    var burger = document.querySelector(".navbar-burger");
+    var nav = document.querySelector(".navbar-menu");
+    burger.classList.toggle("is-active");
+    nav.classList.toggle("is-active");
+  };
+
   return (
     <Navbar fixed="top">
-      <Navbar.Item>
-        <NavLink to="/">Home</NavLink>
-      </Navbar.Item>
-      <Dropdown label="Genres">
-        {genres.map((genre) => {
-          return (
-            <Dropdown.Item
-              renderAs="a"
-              href={`/genres/${genre.id}`}
-              value={genre.name}
-            >
-              {genre.name}
-            </Dropdown.Item>
-          );
-        })}
-      </Dropdown>
+      <Navbar.Burger onClick={toggleBurger} />
+      <Navbar.Menu>
+        <Navbar.Item>
+          <NavLink to="/">Home</NavLink>
+        </Navbar.Item>
+        <Dropdown label="Genres">
+          {genres.map((genre) => {
+            return (
+              <Dropdown.Item
+                renderAs="a"
+                href={`/genres/${genre.id}`}
+                value={genre.name}
+              >
+                {genre.name}
+              </Dropdown.Item>
+            );
+          })}
+        </Dropdown>
 
-      <Form.Input
-        // TODO: Query API for inputted game 'name', 'name_original' or 'alternative_names'
-        type="text"
-        placeholder={`Search over ${gameCount} games...`}
-      />
-      {/* TODO: onClick() query to go the SingleGame page with that game as props (if multiple games match query, just use first)*/}
-      <Button type="submit">Search</Button>
+        <Form.Input
+          // TODO: Query API for inputted game 'name', 'name_original' or 'alternative_names'
+          type="text"
+          placeholder={`Search over ${gameCount} games...`}
+        />
+        {/* TODO: onClick() query to go the SingleGame page with that game as props (if multiple games match query, just use first)*/}
+        <Button type="submit">Search</Button>
 
-      {/* TODO: Only render Profile if user is logged in */}
-      <Navbar.Item>
-        <NavLink to={`/profile/${userID}`}>Profile</NavLink>
-      </Navbar.Item>
-      <Navbar.Item to="/signup">
-        <NavLink to="/signup">Sign up</NavLink>
-      </Navbar.Item>
-      <Navbar.Item to="/login">
-        <NavLink to="/login">Log in</NavLink>
-      </Navbar.Item>
+        {/* TODO: Only render Profile if user is logged in */}
+        <Navbar.Item>
+          <NavLink to={`/profile/${userID}`}>Profile</NavLink>
+        </Navbar.Item>
+        <Navbar.Item to="/signup">
+          <NavLink to="/signup">Sign up</NavLink>
+        </Navbar.Item>
+        <Navbar.Item to="/login">
+          <NavLink to="/login">Log in</NavLink>
+        </Navbar.Item>
+      </Navbar.Menu>
     </Navbar>
   );
 }
